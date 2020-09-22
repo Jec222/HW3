266bcd92816c207318272371fb824c8a190ae172
Jeremias Celidon <jec222@txstate.edu>
2020-09-22 03:19:36 +0000

    testing

--- hw3.cpp
+++ hw3.cpp
@@ -1,6 +1,34 @@
 /****
 * PSEUDOCODE:
-*
+
+function reverse an array 
+
+will start by knowing where to start
+and what the end of the string will be and keep track of them
+will increment start and decrease end untill start is bigger than end
+will store start and replace it with end
+will get the value stored and set it equal to end 
+will decrease end and increase start 
+
+function hasbalancedparantheses
+
+will check is the parathesis are balanced recursebly 
+will check if all paranthesis have pairs size will be 0 and return true else will be false also if we see ) at the begining we return false will treverse if we see a ( will look for ) calling it seft until it finds closing bracket or it finds the end of the string and returns false. 
+
+function DogishHelper 
+
+will look for a charater that you desire 
+will return false of string is empty 
+and true if it finds the character that you are looking for 
+by calling it self over and over again. 
+
+function InDogish 
+
+will check if a for is part of the dogish laguage 
+will call a requesive fuction call InDogish will look for d,o,g
+in that order will have to be in that order. if it finds it we will
+return true else it will return false. 
+
 */
 #include <string>
 void ReverseArray(int arr[], int length){
@@ -18,6 +46,7 @@ void ReverseArray(int arr[], int length){
 }
 
 bool HasBalancedParentheses(std::string input){
+
   char match = ')';
   int count = 0,
           treverse = 0;
@@ -61,7 +90,6 @@ bool DogishHelper(std::string word, char letter){
 }
 bool InDogish(std::string input){
 
-
   if(DogishHelper(input,'d')){
     int new_location = input.find('d');
     if(DogishHelper(input.substr(new_location),'o')){
@@ -69,12 +97,12 @@ bool InDogish(std::string input){
       if(DogishHelper(input.substr(new_location),'g')){
         return true;
       }
-
     }
   }
   return false;
 }
 
 bool InXish(std::string input, std::string choosen_word){
-  return true;
+
+ return true;
 }
